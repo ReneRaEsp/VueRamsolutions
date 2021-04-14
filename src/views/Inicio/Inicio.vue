f0a9c720763149f77f63d167868a8cc2d2fcea1a
Rene <rdejramirez@gmail.com>
2021-04-14 14:33:58 -0500

    k

--- src/views/Inicio/Inicio.vue
+++ src/views/Inicio/Inicio.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="inicio">
     <Seccion1 />
+    <Seccion2 />
+    <Seccion3 />
   </div>
 </template>
 
 <script>
 import Seccion1 from "./components/Seccion1.vue";
+import Seccion2 from "./components/Seccion2.vue";
+import Seccion3 from "./components/Seccion3.vue";
 export default {
   components: {
     Seccion1,
+    Seccion2,
+    Seccion3,
   },
 };
 </script>
